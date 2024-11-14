aade7a62e77721755029cfdeea2ea5902c317cc2
Sneha B <snehabsivakripa@gmail.com>
2024-11-14 12:28:29 -0500

    .

--- src/ArrayorStrings/String_Compression.java
+++ src/ArrayorStrings/String_Compression.java
@@ -2,34 +2,35 @@ package ArrayorStrings;
 
 public class String_Compression {
     public int compress(char[] chars) {
-        StringBuilder str=new StringBuilder();
+        int index=0;
         int count=1;
         if(chars.length==1){
-            str.append(chars[0]);
+            chars[index++]=chars[0];
         }
         for(int i=1;i<chars.length;i++){
-            if(chars[i]==chars[i-1]){
-                count++;
+            if(chars[i]!=chars[i-1]){
+                chars[index++]=chars[i-1];
+                if(count>1){
+                    for(char num:Integer.toString(count).toCharArray())
+                        chars[index++]=num;
+                }
+                count=1;
             }else{
-                if(count!=1) {
-                    str.append(chars[i - 1] + String.valueOf(count));
-                    count = 1;
-                }else{
-                    str.append(chars[i-1]);
+                count++;
+                if(i==chars.length-1){
+                    chars[index++]=chars[i-1];
+                    if(count>1){
+                        for(char num:Integer.toString(count).toCharArray())
+                            chars[index++]=num;
+                    }
                 }
             }
-            if(i==chars.length-1 && count!=1){
-                str.append(chars[i - 1] + String.valueOf(count));
-            }
-        }
-        char[] compressed=str.toString().toCharArray();
-        int j=0;
-        for(char c:compressed){
-            chars[j]=c;
-           j++;
+
 
         }
-        return compressed.length;
+
+        System.out.println(chars);
+        return index;
     }
 
     public static void main(String[] args) {
